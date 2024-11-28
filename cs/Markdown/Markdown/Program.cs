7bb8c846c7792ad265312617d0a9043a64ef7f98
maakhhh <d.makhlonov@mail.ru>
2024-11-29 02:43:07 +0500

    добавил Di container

--- cs/Markdown/Markdown/Program.cs
+++ cs/Markdown/Markdown/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace Markdown;
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Markdown;
 
 public static class Program
 {
     public static void Main()
     {
+        var services = new ServiceCollection();
 
+        services.AddSingleton<IMarkdownConverter, MarkdownToHtmlConverter>();
+        services.AddSingleton<IMarkdownParser, MarkdownParser>();
+        services.AddSingleton<ITokenParser, BoldTokenParser>();
+        services.AddSingleton<ITokenParser, HeaderTokenParser>();
+        services.AddSingleton<ITokenParser, ItalicTokenParser>();
+        services.AddSingleton<Md>();
+
+        var sp = services.BuildServiceProvider();
+        var md = sp.GetService<Md>();
     }
 }
